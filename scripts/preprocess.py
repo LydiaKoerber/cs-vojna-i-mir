a940ebe3f7c37ca80d6a9547c97c24d644a6ead9
LydiaKoerber <lydi.koerber@web.de>
2024-09-30 11:57:22 +0200

    parse by sentence, not paragraph

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -16,10 +16,8 @@ def has_latin(text):
 
 
 def create_df(data, part_number, output_dir='../outputs/'):
-    column_names = ['volume', 'part', 'chapter', 'text', 'number_tokens', 'cs',
-                    'num-switches-intraturn', 'num-switches-interturn',
-                    'num-switches-intrasent', 'num-switches-intraword', 
-                    'num_sent', 'len_sents', 'len_cs_fr', 'len_cs_ru']
+    column_names = ['volume', 'part', 'chapter', 'paragraph', 'num_sent', 'num_interturn', 'num_intersent',
+                                    'tokens', 'len_sent', 'cs', 'cs_indices', 'num_intrasent', 'maj_lang', 'first_lang', 'last_lang', 'embedded']
     df = pd.DataFrame(data, columns=column_names)
     if output_dir:
         df.to_csv(f'{output_dir}cs_{part_number}.csv', encoding='utf-8')
@@ -42,95 +40,83 @@ def parse_file(path):
             part_number = i + 1
             chapters = [x for x in re.split(r'\n[XVIM]+\.\n', p)
                         if re.match('\s+', x) and x.strip() != ""]
-            # chapters.append(chapters_new)
             for j, c in enumerate(chapters):
-                # if c.strip() == "": 
-                #     continue
                 # prevent 0-indexing
                 chapter_number = j + 1
                 lines = c.split('\n\n')
-                for l in lines:
-                    tokens, cs, num_intrasent, num_intraword, num_sent, len_sents, len_cs_fr, len_cs_ru, no_tokens = parse_line(l)#, speaker=speaker, letter=letter)
+                for i, l in enumerate(lines):
+                    num_interturn = 0
+                    sents, num_sent, num_intersent = parse_sents(l.strip())
+                    # tokens, cs, num_intrasent, num_intraword, num_sent, len_sents, len_cs_fr, len_cs_ru, no_tokens = parse_line(l)
                     # include CS instances only
-                    if 'non-ru' in cs:
-                        num_intra_turn_switches = 0
-                        num_inter_turn_switches = 0
-                        lang = ''
-                        for s in cs:
-                            if not s:  # empty string, punctuation or other
-                                continue
-                            if s != lang:
-                                if not lang == '':
-                                    num_intra_turn_switches += 1
-                                lang = s
-                        if num_intra_turn_switches == 0:
-                            num_inter_turn_switches = 1
-                        data.append([volume_number, part_number, chapter_number,
-                                    tokens, no_tokens, cs, num_intra_turn_switches,
-                                    num_inter_turn_switches, num_intrasent,
-                                    num_intraword, num_sent, len_sents,
-                                    len_cs_fr, len_cs_ru])
+                    for j, sent in enumerate(sents):
+                        tokens, cs, cs_indices, maj_lang, len_sent, first_lang, last_lang, num_intrasent = sent
+                        if j == 0 and data:
+                            # check last language of previous turn to detect
+                            # inter-turn switches
+                            if data[-1][-1] != first_lang:
+                                num_interturn += 1
+                        if first_lang == last_lang and num_intrasent > 1:
+                            embedded = True
+                        else:
+                            embedded = False
+                        data.append([volume_number, part_number, chapter_number, i, num_sent, num_interturn, num_intersent,
+                                    tokens, len_sent, cs, cs_indices, num_intrasent, maj_lang, first_lang, last_lang, embedded])
     create_df(data, volume_number)
     return data
 
 
-def parse_line(l, nlp=nlp_mul):
-    tokens, cs = [], []
+def parse_sents(s, nlp=nlp_mul):
+    tokens, cs, cs_indices = [], [], []
+    sents = []
     # remove footnote markers, e.g. [8]
-    l = re.sub(r"\[\d+\]", "", l)
-    parsed = nlp(l)
-    num_intrasent, num_intraword, num_sent = 0, 0, 0
-    len_sents = []
-    len_cs_fr, len_cs_ru = [], []
-    lcsf, lcsr = 0, 0
+    s = re.sub(r"\[\d+\]", "", s)
+    parsed = nlp(s)
+    num_sent = 0
+    num_intersent = 0
     for sent in parsed.sents:
+        num_intrasent = 0
         num_sent += 1
-        len_sents.append(len(sent))
-        sent_txt = ' '.join([token.text for token in sent])
-        if has_cyrillic(sent_txt) and has_latin(sent_txt):
-            num_intrasent += 1
-        for token in sent:
+        len_sent = len(sent)
+        current_lang = ''
+        for i, token in enumerate(sent):
             tokens.append(token.text)
-            if has_cyrillic(token.text) and has_latin(token.text):
-                num_intraword += 1
-                print(token.text, sent_txt)
             if has_cyrillic(token.text):
                 cs.append('ru')
-                lcsr += 1
-                if lcsf != 0:
-                    len_cs_fr.append(lcsf)
-                lcsf = 0
             # TODO actual lang detect
             elif has_latin(token.text):
                 cs.append('non-ru')
-                lcsf += 1
-                if lcsr != 0:
-                    len_cs_ru.append(lcsr)
-                lcsr = 0
             else:
                 cs.append('')
-    last_language = next((item for item in reversed(cs) if item != ''), None)
-    if lcsf != 0 and last_language == 'non-ru':
-        len_cs_fr.append(lcsf)
-    if lcsr != 0 and last_language == 'ru':
-        len_cs_ru.append(lcsr)
-    no_tokens = len(tokens)
+            if cs[-1] == '':
+                continue
+            if current_lang == '':
+                current_lang = cs[-1]
+            # intra sentential switch detected
+            elif current_lang != cs[-1]:
+                cs_indices.append(i)
+                num_intrasent += 1
+                current_lang = cs[-1]
+        assert len(cs_indices) == num_intrasent
+        # majority language based on count
+        if cs.count('ru') > cs.count('non-ru'):
+            maj_lang = 'ru'
+        elif cs.count('ru') < cs.count('non-ru'):
+            maj_lang = 'non-ru'
+        else:  # equal
+            maj_lang = 'equal'
+        first_lang = next((item for item in cs if item != ''), None)
+        last_lang = next((item for item in reversed(cs) if item != ''), None)
+        if sents:
+            # check for intersentential switch, last language of previous sent
+            if first_lang != sents[-1][-2]:
+                num_intersent += 1
+        sents.append([tokens, cs, cs_indices, maj_lang, len_sent, first_lang, last_lang, num_intrasent])
+        tokens, cs, cs_indices = [], [], []
+    return sents, num_sent, num_intersent
 
-    assert sum(len_cs_fr) + sum(len_cs_ru) == no_tokens - cs.count('')
-    return tokens, cs, num_intrasent, num_intraword, num_sent, len_sents, len_cs_fr, len_cs_ru, no_tokens
-
+   
 # TODO cs instanzen nach länge auszählen und dann POS/DEP kombis vergleichen
-
-def detect_direct_speech(tokens):
-    ds = []
-    if tokens[0] == '«':
-        pass
-    return ds
-
-
-def tokenize(line):
-    return
-
 
 
 if __name__ == '__main__':
@@ -138,3 +124,5 @@ if __name__ == '__main__':
     for f in sorted(os.listdir(corpus_dir)):
         print(f)
         parse_file(corpus_dir+f)
+    # s = "— Ну, вот чтò, господа, — сказал Билибин, — Болконский мой гость в доме и здесь в Брюнне, и я хочу его угостить, сколько могу, всеми радостями здешней жизни. Ежели бы мы были в Вене, это было бы легко; но здесь, dans ce vilain trou morave,[290] это труднее, и я прошу у всех вас помощи. Il faut lui faire les honneurs de Brünn.[291] Вы возьмите на себя театр, я — общество, вы, Ипполит, разумеется, — женщин."
+    # print(parse_sents(s))
